43fb6785edb1802d55d35eb8d4a056d1986ce262
saniyad-hussain <chowdhurysaniayd@gmail.com>
2020-08-22 14:02:31 -0400

    random user api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    .then(data => setUsers(data.results))
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {users.map(user =>
+           <RandomPeople
+            profile={user.picture.large}
+            fname={user.name.first} 
+            lname={user.name.last}
+            gender={user.gender} 
+            email={user.email}>
+           </RandomPeople>)}
       </header>
     </div>
   );
+}
+
+function RandomPeople(props){
+  const imgStyle = {
+    borderRadius : '50%',
+  }
+  return(
+    <div>
+      <img style={imgStyle} src={props.profile} alt=""/>
+      <h3>Name: {props.fname} {props.lname}</h3>
+      <p>Gender: {props.gender}</p>
+      <p>Email: {props.email}</p>
+    </div>
+  )
 }
 
 export default App;
